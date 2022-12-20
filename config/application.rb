2b2a2cf619d644b0451e88f4d9a145127805834d
ishrat-jahan-swarna <ishrat@nascenia.com>
2022-12-20 17:19:04 +0600

    internationalization

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require "i18n-js"
+
+
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,5 +20,8 @@ module Depot
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # config.middleware.use I18n::Middleware
+    # config.middleware.use SimplesIdeias::I18n::Middleware
+    # config.middleware.use I18n::JS::Middleware
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,19 @@ Rails.application.routes.draw do
   end
   get 'sessions/create'
   get 'sessions/destroy'
+
+  # START_HIGLIGHT
+  resources :support_requests, only: [ :index, :update ]
+  # END_HIGLIGHT
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
+  end
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
